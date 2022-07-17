1764898766e873b85fbcf0e78805b5a21b672662
realten <wonyj210@gmail.com>
2022-07-18 01:00:20 +0900

    2022.07.16 commit
    중간 반영

--- frontservice/src/main/java/com/datasolution/msa/frontservice/controller/HomeController.java
+++ frontservice/src/main/java/com/datasolution/msa/frontservice/controller/HomeController.java
@@ -2,14 +2,19 @@ package com.datasolution.msa.frontservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * HomeController
- */
 @Controller
+@RequestMapping("/")
 public class HomeController {
     @GetMapping("/")
-    public String homeView() {
+    public String homeView(){
         return "home";
     }
+
+    @GetMapping("/style-sample")
+    public String styleSampleView() {
+        return "sample/style-sample";
+    }
+
 }
